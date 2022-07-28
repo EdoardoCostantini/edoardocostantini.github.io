e9207e54ee0b595d3d354d285875f8c2a6d1e73d
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-07-28 15:14:33 +0200

    automatic website update

--- post/residual-standard-error/se-resid.R
+++ post/residual-standard-error/se-resid.R
@@ -24,7 +24,37 @@
     # Compare with the manual computation
     sigma(lm_fit) - rse
 
-# Check other things
+# Check other computation of degrees of freedom
 
+    lm_fit$df.residual
+    summary(lm_fit)$df[2]
     df.residual(lm_fit)
-    (n - k - 1)
+    n - k - 1
+
+# Center the dependent variable
+
+    y <- mtcars[, 1] # mpg
+    X <- as.matrix(mtcars[, -1])
+    n <- nrow(X)
+    p <- ncol(X)
+
+    # Regular LM fit with intercept
+    lm.fit <- lm(y ~ X)
+    lm.fit$df.residual
+    n - p - 1
+
+    # Centered Y
+    lm.fit.sc <- lm(scale(y, center = TRUE, scale = FALSE) ~ - 1 + X)
+    lm.fit.sc$df.residual
+    n - p
+
+    # Compare gits
+    summary(lm.fit)$r.squared
+    summary(lm.fit.sc)$r.squared
+
+    # Compute rse
+    rse <- sqrt(sum((y - mean(y) - fitted(lm.fit.sc) )^2) / (n - p))
+    rse
+
+    # Use the sigma function to extract it from an lm object
+    sigma(lm.fit.sc)
